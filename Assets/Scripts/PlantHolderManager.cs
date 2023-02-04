2e610442e49ec19eb4e0afe29c68b49ae87d98a9
Bowen <1758364810@qq.com>
2023-02-04 14:43:52 -0500

    updated plant holders scripts (main part finished)

--- Assets/Scripts/PlantHolderManager.cs
+++ Assets/Scripts/PlantHolderManager.cs
@@ -6,53 +6,68 @@ public class PlantHolderManager : MonoBehaviour
 {
     public GameObject[] PlantSpritesList;
 
-    private int PlantType = 0; // Plant sprites are decided by type, status and stage
-    private int PlantStage = 0;
-    private int PlantStatus = 0; // 0 - growing, 1 - stop growing (event happens), 2 - mature, 3 - illness, 4 - die
-    private int TimeRemainToNextStage = -1;
+    public int PlantType = -1; // Plant sprites are decided by type, status and stage
+    public int PlantStage = 0;
+    public int PlantStatus = 0; // 0 - growing, 1 - stop growing (event happens), 2 - mature, 3 - illness, 4 - die
+    public int TimeRemainToNextStage = 0;
 
     PlantObjectProperty plantProperty = null;
-    SpriteRenderer p_SpriteRenderer;
+    SpriteRenderer stem_SpriteRenderer;
+    SpriteRenderer root_SpriteRenderer;
+
+    public GameObject rootHolder;
 
     // Start is called before the first frame update
     void Start()
     {
-        p_SpriteRenderer = GetComponent<SpriteRenderer>();
+        stem_SpriteRenderer = this.GetComponent<SpriteRenderer>();
+        root_SpriteRenderer = rootHolder.GetComponent<SpriteRenderer>();
+        addNewPlant(0);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (PlantStatus == 0)
+        if (PlantType != -1)
         {
-            if (TimeRemainToNextStage > 0) // time minus
+            if (PlantStatus == 0)
             {
-                TimeRemainToNextStage--;
-            }
-            else if (TimeRemainToNextStage == 0) // grow to next stage
-            {
-                if (PlantStage < 4)
+                if (TimeRemainToNextStage > 0) // time minus
                 {
-                    PlantStage++;
-                    TimeRemainToNextStage = plantProperty.getStageTime();
-                    p_SpriteRenderer.sprite = plantProperty.getSprite(0, PlantStage, 0);
+                    Debug.Log(TimeRemainToNextStage);
+                    TimeRemainToNextStage--;
                 }
-                else
+                else if (TimeRemainToNextStage == 0) // grow to next stage
                 {
-                    TimeRemainToNextStage = -1;
-                    PlantStatus = 2;
+                    if (PlantStage < 4)
+                    {
+                        PlantStage++;
+                        TimeRemainToNextStage = plantProperty.getStageTime();
+                        stem_SpriteRenderer.sprite = plantProperty.getSprite(0, PlantStage, 0);
+                        root_SpriteRenderer.sprite = plantProperty.getSprite(1, PlantStage, 0);
+                    }
+                    else
+                    {
+                        TimeRemainToNextStage = -1;
+                        PlantStatus = 2;
+                    }
                 }
+                else { } // mature or die
+
             }
-            else { } // mature or die
+            //GetComponent<SpriteRenderer>() = p_SpriteRenderer;
 
         }
-        //GetComponent<SpriteRenderer>() = p_SpriteRenderer;
     }
 
-    void addNewPlant(int type) {
+    public void addNewPlant(int type) {
 
         plantProperty = PlantSpritesList[type].GetComponent<PlantObjectProperty>();
         PlantType = type;
+        TimeRemainToNextStage = plantProperty.getStageTime();
+        PlantStage = 0;
+        stem_SpriteRenderer.sprite = plantProperty.getSprite(0, PlantStage, 0);
+        root_SpriteRenderer.sprite = plantProperty.getSprite(1, PlantStage, 0);
     }
 }
