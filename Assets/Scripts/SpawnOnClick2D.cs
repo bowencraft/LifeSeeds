35f1ea4b16d455e91f09f341c5658f57a0fc4078
Steven Quacles <Sttrevens@126.com>
2023-02-05 03:37:51 -0500

    Planting

---
+++ Assets/Scripts/SpawnOnClick2D.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnOnClick2D : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject[] spawnPoints;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D collider = Physics2D.OverlapPoint(mousePosition);
+            foreach (GameObject spawnPoint in spawnPoints)
+            {
+                if (collider == spawnPoint.GetComponent<Collider2D>())
+                {
+                    Instantiate(prefab, spawnPoint.transform.position, Quaternion.identity);
+                    break;
+                }
+            }
+        }
+    }
+}
